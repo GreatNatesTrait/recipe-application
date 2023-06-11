a539c5f6469d94a4225e3750c74707565218aa9c
GreatNatesTrait <mclawhornnathan@gmail.com>
2023-06-11 14:23:25 -0400

    corrected cors syntax

--- server/api/lambda-functions/dynamo-API/terraform/main.tf
+++ server/api/lambda-functions/dynamo-API/terraform/main.tf
@@ -82,14 +82,14 @@ EOF
 resource "aws_apigatewayv2_api" "dynamo_api" {
   name        = "dynamo-api"
   protocol_type = "HTTP"
-  cors_configuration = [{
+  cors_configuration  {
     allow_credentials = false
     allow_headers     = ["*"]
     allow_methods     = ["*"]
     allow_origins     = ["*"]
     expose_headers    = ["*"]
     max_age           = 0
-  }]
+  }
 }
 
 resource "aws_apigatewayv2_stage" "default" {
