8587e381e2f05409297778ee72293267562b0782
GreatNatesTrait <mclawhornnathan@gmail.com>
2023-06-15 21:05:02 -0400

    lambda for logging to s3

--- server/api/lambda-functions/logger-API/code/main.js
+++ server/api/lambda-functions/logger-API/code/main.js
@@ -2,39 +2,41 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const lambda_handler = async (event) => {
   try {
-    // Parse the request body from API Gateway
+    const s3Client = new S3Client({ region: "us-east-1" });
     const { body } = event;
-    const { logData } = JSON.parse(body);
+    const requestBody = JSON.parse(body);
 
-    // Create an instance of the S3 client
-    const s3Client = new S3Client({ region: 'us-east-1' }); // Replace with your desired region
+    const bucketName = "recipe-app-code";
+    
+    //const decodedFile = Buffer.from(requestBody, "base64");
+    const now = new Date();
+    const milliseconds = now.getMilliseconds();
+    const timestamp = now.toISOString().replace('T', ' ').replace('Z', '');
 
-    // Set the S3 bucket name and key
-    const bucketName = 'recipe-app-code'; // Replace with your S3 bucket name
-    const key = 'logs/logfile.txt'; // Replace with your desired S3 bucket key/path
+    const fileTimestamp =  `${timestamp}.${milliseconds}`;
+    const key = `logs/${fileTimestamp} logfile.txt`;
 
-    // Prepare the parameters for the S3 PutObject command
     const params = {
       Bucket: bucketName,
       Key: key,
-      Body: logData,
+      //Body: decodedFile
+      Body: requestBody,
     };
 
-    // Execute the S3 PutObject command
     const command = new PutObjectCommand(params);
     await s3Client.send(command);
 
     // Return a success response
     return {
       statusCode: 200,
-      body: 'Log data written to S3',
+      body: "Log data written to S3",
     };
   } catch (error) {
-    // Return an error response if any exception occurs
+      console.log(error)
     return {
       statusCode: 500,
       body: JSON.stringify({
-        message: 'Error writing log data to S3',
+        message: "Error writing log data to S3",
         error: error.message,
       }),
     };
