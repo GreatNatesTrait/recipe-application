f5eb3971524d43d24b6a20982b233447dcf59d04
GreatNatesTrait <mclawhornnathan@gmail.com>
2023-06-23 10:38:47 -0400

    removed duplicate route

--- server/api/lambda-functions/dynamo-API/terraform/main.tf
+++ server/api/lambda-functions/dynamo-API/terraform/main.tf
@@ -133,12 +133,6 @@ resource "aws_apigatewayv2_route" "Get_Recipe_By_ID" {
   target    = "integrations/${aws_apigatewayv2_integration.recipe_app.id}"
 }
 
-resource "aws_apigatewayv2_route" "Get_Recipe_By_ID" {
-  api_id = aws_apigatewayv2_api.dynamo_api.id
-  route_key = "GET /recipe"
-  target    = "integrations/${aws_apigatewayv2_integration.recipe_app.id}"
-}
-
 resource "aws_apigatewayv2_route" "Delete_Recipe_By_ID" {
   api_id = aws_apigatewayv2_api.dynamo_api.id
   route_key = "DELETE /recipe"
